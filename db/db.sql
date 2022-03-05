a220bf96231e24ad0532c49c38a15edaad341f7c
TennCreekBridges <bridgettvalet@gmail.com>
2022-03-05 15:03:56 -0600

    adds foreign key to schema

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,5 @@
 DROP DATABASE IF EXISTS employees;
 CREATE DATABASE employees;
 USE employees;
+
+source db/schema.sql;
--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,28 @@
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
+
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
-);
+    name VARCHAR(30))
+;
 
 CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL(10, 2),
-    department_id INTEGER
-);
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL)
+;
 
 CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name varchar(30),
     role_id INTEGER,
-    manager_id INTEGER
-);
+    manager_id INTEGER,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL)
+;
+
+source db/seeds.sql;
