065279543c098e58f4974578fbe71231074fda22
TennCreekBridges <bridgettvalet@gmail.com>
2022-03-05 14:25:36 -0600

    adds express install

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
-    salary DECIMAL,
+    salary DECIMAL(10, 2),
     department_id INTEGER
 );
 
